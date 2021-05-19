309315dc53d8c936c8cd6623c950df72d46713ad
Ng Jia Yuan <ngjiayuan.z@gmail.com>
2021-05-19 17:31:49 +0800

    initial commit

---
+++ src/const.js
@@ -0,0 +1,26 @@
+export const users = [
+    {
+        id: 1,
+        name: 'Jia Yuan',
+        username: 'jiayuan',
+        quote: 'I am your father'
+    },
+    {
+        id: 2,
+        name: 'Gu Geng',
+        username: 'gugeng',
+        quote: 'Hello'
+    },
+    {
+        id: 3,
+        name: 'Charlton',
+        username: 'charlton',
+        quote: 'im gna slap u'
+    },
+    {
+        id: 4,
+        name: 'Zach',
+        username: 'zach',
+        quote: 'My bio'
+    }
+]
